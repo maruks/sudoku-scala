da6addc2122fff1a2cf872d2d34ee241f4479936
Maris Orbidans <maris@MarrutkaMaks.local>
2011-04-15 08:35:37 +0100

    code

--- src/main/scala/sudoku/SudokuSolver.scala
+++ src/main/scala/sudoku/SudokuSolver.scala
@@ -1,7 +1,36 @@
 object SudokuSolver {
 
-  def solve(puzzle: Vector[Int]):Vector[Int] = {
-    puzzle
+  val numbers = Set(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+  def solve(p: Vector[Int]): Vector[Int] = {
+    val set = findChangeSet(p)
+    if (set._2.isEmpty) {
+      Vector.empty[Int]
+    } else {
+      val iter = set._2.iterator
+      var result = p
+      do {
+        result = solve(p.updated(set._1, iter.next))
+      } while (result.isEmpty && iter.hasNext)
+      result
+    }
   }
 
+  def findChangeSet(p: Vector[Int]): (Int, Set[Int]) = {
+    val sets = for (i <- 0 to 80; if (p(i) == 0))
+      yield (i, numbers -- (selectRow(p, i) ++ selectColumn(p, i) ++ selectSquare(p, i)))
+    sets.reduceLeft((a, b) => if (a._2.size < b._2.size) a else b)
+  }
+
+  def selectRow(p: Vector[Int], i: Int): Set[Int] = {
+    numbers
+  }
+
+  def selectColumn(p: Vector[Int], i: Int): Set[Int] = {
+    numbers
+  }
+
+  def selectSquare(p: Vector[Int], i: Int): Set[Int] = {
+    numbers
+  }
 }
