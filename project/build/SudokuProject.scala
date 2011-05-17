42b80a11a9de959a76867959d8bb5a3ba233e046
maris <maris@maris-ThinkPad-T61.(none)>
2011-05-18 00:31:09 +0100

    scala 2.9

--- project/build/SudokuProject.scala
+++ project/build/SudokuProject.scala
@@ -2,6 +2,6 @@ import sbt._
 
 class SudokuProject(info: ProjectInfo) extends DefaultProject(info) with assembly.AssemblyBuilder
 {
-  val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test->default"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test->default"
   val junit = "junit" % "junit" % "4.8.2" % "test->default"
 }
