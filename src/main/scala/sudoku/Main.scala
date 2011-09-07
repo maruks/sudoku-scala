157a97094790aec6b55f6f53ba09394bcb442f3f
Maris Orbidans <Maris.Orbidans@gmail.com>
2011-09-07 02:01:35 +0100

    batch mode added

--- src/main/scala/sudoku/Main.scala
+++ src/main/scala/sudoku/Main.scala
@@ -3,13 +3,20 @@ package sudoku
 import java.io.File
 
 object Main {
-  def main(args: Array[String]):Unit = {
-    if (args.length!=1) {
-      println("Usage SudokuSolver <filename>")
-      System.exit(1)
+  def main(args: Array[String]): Unit = {
+
+    args match {
+      case Array(f) => {
+        val p = SudokuIO.readFile(new File(f))
+        val sol = SudokuSolver.solve(p)
+        SudokuIO.writeFile(System.out, sol)
+      }
+      case Array(f, a) => {
+	val p = SudokuIO.readBatchFile(new File(f)).par
+	val s = p.map(SudokuSolver.solve(_))
+	s.foreach(SudokuIO.writeFile(System.out, _))
+      }
+      case _ => println("Usage SudokuSolver <filename>")
     }
-    val p = SudokuIO.readFile(new File(args(0)))
-    val sol = SudokuSolver.solve(p)
-    SudokuIO.writeFile(System.out,sol)
   }
 }
--- src/main/scala/sudoku/SudokuIO.scala
+++ src/main/scala/sudoku/SudokuIO.scala
@@ -9,19 +9,23 @@ object SudokuIO {
   def readFile(file: File): Vector[Int] = {
     val b = new VectorBuilder[Int]()
     b.sizeHint(9 * 9)
-    Source.fromFile(file).getLines().foreach(line => b ++= line.split(',').map(s => Integer.parseInt(s.trim)))
-    b.result()
+    Source.fromFile(file).getLines.foreach(line => b ++= line.split(',').map(s => Integer.parseInt(s.trim)))
+    b.result
+  }
+
+  def readBatchFile(file: File): Iterable[Vector[Int]] = {
+    val lines = Source.fromFile(file).getLines
+    lines.map(line => line.trim.map(c => Integer.parseInt(c.toString)).asInstanceOf[Vector[Int]]).toIterable
   }
 
   def writeFile(os: OutputStream, v: Option[Vector[Int]]) = {
-    val out = new OutputStreamWriter(os)
+    val out = new PrintWriter(os,true)
     v match {
       case None => out.write("no solution")
       case Some(s) => {
         val sol = for (g <- s.grouped(9).toList; str = (g.head.toString /: g.tail)(_ + ',' + _)) yield str
-        out.write((sol.head /: sol.tail)(_ + '\n' + _))
+        out.println((sol.head /: sol.tail)(_ + '\n' + _) + '\n')
       }
     }
-    out.close
   }
 }
