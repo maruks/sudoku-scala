98bfe2984c0157d2f81cea54230374892f09bd34
Maris Orbidans <maris.orbidans@gmail.com>
2012-01-16 14:04:03 +0000

    list comprehensions replaced with map/filter

--- src/main/scala/sudoku/SudokuSolver.scala
+++ src/main/scala/sudoku/SudokuSolver.scala
@@ -7,24 +7,27 @@ object SudokuSolver {
       case None => Some(p)
       case Some(set) if set._2.isEmpty => None
       case Some(set) => {
-        val sol = for (i <- set._2.toSeq.sorted.view) yield solve(p.updated(set._1, i))
+        val sol = set._2.toSeq.sorted.view.map(i => solve(p.updated(set._1, i)))
         sol.find(_.isDefined).getOrElse(None)
       }
     }
   }
 
   def smallestChangeSet(p: Vector[Int]): Option[(Int, Set[Int])] = {
-    val sets = for (i <- 0 to p.size - 1; if (p(i) == 0))
-      yield (i, (1 to 9).toSet -- (selectRow(p, i) ++ selectColumn(p, i) ++ selectSquare(p, i)))
+    val sets = (0 to p.size - 1).filter(p(_) == 0).map( i => (i, possibleValues(p,i)))
     if (sets.isEmpty) None else Some(sets.reduceRight((a, b) => if (a._2.size < b._2.size) a else b))
   }
 
+  def possibleValues(p: Vector[Int], i: Int) = {
+    (1 to 9).toSet -- (selectRow(p, i) ++ selectColumn(p, i) ++ selectSquare(p, i))
+  }
+
   def selectRow(p: Vector[Int], i: Int) = {
-    for (i <- (i - i % 9 to i - i % 9 + 8).toSet[Int]) yield p(i)
+    (i - i % 9 to i - i % 9 + 8).map(p(_)).toSet[Int]
   }
 
   def selectColumn(p: Vector[Int], i: Int) = {
-    for (i <- (i % 9 to i % 9 + 72 by 9).toSet[Int]) yield p(i)
+    (i % 9 to i % 9 + 72 by 9).map(p(_)).toSet[Int]
   }
 
   def selectSquare(p: Vector[Int], i: Int) = {
@@ -32,6 +35,6 @@ object SudokuSolver {
     val y = i / 9 - (i / 9) % 3
     val spos = x + y * 9
     val r = (spos to spos + 2)
-    for (i <- (r ++ r.map(_ + 9) ++ r.map(_ + 18)).toSet[Int]) yield p(i)
+    (r ++ r.map(_ + 9) ++ r.map(_ + 18)).map(p(_)).toSet[Int]
   }
 }
